0e58eab5ae0cf8be564c35b9262c9c605fa9ed81
bprasad156 <156602266+bprasad156@users.noreply.github.com>
2024-05-20 18:44:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
-}
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+    // Write your code here
+    
+    for(var i=0;i<str.length;i++){
+        var s=str.charAt(i)
+         if(s.IndexOf(c)===i&&s.IndexOf(c)===-1){
+            return c;
+
+         }
+    }
+    return null;
+       
+   }
+   const input = prompt("Enter a string");
+   alert(firstNonRepeatedChar(input)); 
+   
